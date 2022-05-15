875220b2a696a880c4e955fd86211bf43196cc35
Leonid Lichkun <lionproklon@gmail.com>
2022-05-15 22:06:00 +0300

    Added form to add point to the map

--- src/pages/pointAdd.tsx
+++ src/pages/pointAdd.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React from "react";
 
 interface PointAddProps {
@@ -5,12 +6,56 @@ interface PointAddProps {
 }
 
 const PointAdd: React.FC<PointAddProps> = (props: PointAddProps) => {
+  const { onSubmit } = props;
   return (
-    <div>
-      Вкладка для добавления точки, на которой вводится название, описание точки
-      и кликом по карте (предпочтительный вариант) или в просто в полях ввода
-      указываются ее координаты
-    </div>
+    <Box
+      component="form"
+      onSubmit={onSubmit}
+      sx={{
+        boxShadow: 16,
+        width: "600px",
+        p: 5,
+        mx: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}>
+      <Typography variant="h4" sx={{ textAlign: "center", mb: 2 }}>
+        Добавить точку на карту
+      </Typography>
+      <TextField margin="normal" label="Название точки" variant="filled" />
+      <TextField
+        margin="normal"
+        label="Описание точки"
+        variant="filled"
+        multiline
+      />
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h5">Введите координаты</Typography>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <TextField
+            sx={{ width: "45%" }}
+            label="Широта"
+            variant="filled"
+            margin="normal"
+          />
+          <TextField
+            sx={{ width: "45%" }}
+            label="Долгота"
+            variant="filled"
+            margin="normal"
+          />
+        </Box>
+      </Box>
+      <Button
+        variant="contained"
+        type="submit"
+        sx={{
+          mt: 2,
+        }}>
+        Добавить
+      </Button>
+    </Box>
   );
 };
 export default PointAdd;
