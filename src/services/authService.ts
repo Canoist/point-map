73dd79624393fa13b7e327dcb4757f0eb4f3bb45
Leonid Lichkun <lionproklon@gmail.com>
2022-05-23 19:38:36 +0300

    Added types to auth- and httpService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,30 +3,34 @@ import configFile from "../config.json";
 import localStorageService from "./localStorageService";
 
 const httpAuth = axios.create({
-  baseURL: configFile.apiEndPoint + "/auth/",
-  params: { key: process.env.REACT_APP_FIREBASE_KEY },
+    baseURL: configFile.apiEndPoint + "/auth/",
+    params: { key: process.env.REACT_APP_FIREBASE_KEY },
 });
 
+interface IEmailAndPassword {
+    [key: string]: string;
+}
+
 const authService = {
-  register: async (payload) => {
-    const { data } = await httpAuth.post("signUp", payload);
-    return data;
-  },
-  login: async ({ email, password }) => {
-    const { data } = await httpAuth.post("signInWithPassword", {
-      email,
-      password,
-      returnSecureToken: true,
-    });
-    return data;
-  },
-  refresh: async () => {
-    const { data } = await httpAuth.post("token", {
-      grant_type: "refresh_token",
-      refresh_token: localStorageService.getRefreshToken(),
-    });
-    return data;
-  },
+    register: async (payload: string) => {
+        const { data } = await httpAuth.post("signUp", payload);
+        return data;
+    },
+    login: async ({ email, password }: IEmailAndPassword) => {
+        const { data } = await httpAuth.post("signInWithPassword", {
+            email,
+            password,
+            returnSecureToken: true,
+        });
+        return data;
+    },
+    refresh: async () => {
+        const { data } = await httpAuth.post("token", {
+            grant_type: "refresh_token",
+            refresh_token: localStorageService.getRefreshToken(),
+        });
+        return data;
+    },
 };
 
 export default authService;
