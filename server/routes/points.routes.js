673ae1c8474e227c2b2daa821a730cca021ffb02
Leonid Lichkun <lionproklon@gmail.com>
2022-05-29 23:12:38 +0300

    added routes on server side

---
+++ server/routes/points.routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const Point = require("../models/Point");
+const auth = require("../middleware/auth.middleware");
+
+const router = express.Router({ mergeParams: true });
+
+router.patch("/:pointId", auth, async (req, res) => {
+    try {
+        const { pointId } = req.params;
+        if (pointId === req.points._id) {
+            const updatePoint = await Point.findByIdAndUpdate(
+                pointId,
+                req.body,
+                {
+                    new: true,
+                }
+            );
+            res.send(updatePoint);
+        } else {
+            res.status(401).json({ message: "Unauthorized" });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "На сервере произошла ошибка. Попробуйте позже",
+        });
+    }
+});
+
+router.get("/", auth, async (req, res) => {
+    try {
+        const points = await Point.find();
+        res.send(points);
+    } catch (error) {
+        res.status(500).json({
+            message: "На сервере произошла ошибка. Попробуйте позже",
+        });
+    }
+});
+
+module.exports = router;
